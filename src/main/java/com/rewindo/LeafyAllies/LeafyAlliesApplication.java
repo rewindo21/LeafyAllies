ff4033ec189fb5987c91d8098fe253cbc5c1b45f
Amirhossein Andouhgin <amirhossein.andoohgin@gmail.com>
2023-10-03 16:42:20 +0330

    import

---
+++ src/main/java/com/rewindo/LeafyAllies/LeafyAlliesApplication.java
@@ -0,0 +1,13 @@
+package com.rewindo.LeafyAllies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafyAlliesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafyAlliesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rewindo/LeafyAllies/LeafyAlliesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rewindo.LeafyAllies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafyAlliesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
