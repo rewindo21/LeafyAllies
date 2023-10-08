74b5db70f8af9b2dfb110ff6fe282500c24ed700
Amirhossein Andouhgin <amirhossein.andoohgin@gmail.com>
2023-10-08 17:30:43 +0330

    movie class

---
+++ src/main/java/com/rewindo/LeafyAllies/Movie.java
@@ -0,0 +1,26 @@
+package com.rewindo.LeafyAllies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collation = "movies")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+    @Id
+    private ObjectId objectId;
+    private String imdbId;
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String posterLink;
+    private List<String> genres;
+    private List<String> backdrops;
+}
