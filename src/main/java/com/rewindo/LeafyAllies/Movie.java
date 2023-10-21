cf16f700d5041bef9e9ad174fbb2b9e9b51d5a2a
Amirhossein Andouhgin <amirhossein.andoohgin@gmail.com>
2023-10-22 00:22:53 +0330

    movie layers

--- src/main/java/com/rewindo/LeafyAllies/Movie.java
+++ src/main/java/com/rewindo/LeafyAllies/Movie.java
@@ -10,18 +10,18 @@ import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
 import java.util.List;
 
-@Document(collation = "movies")
+@Document(collection = "movies")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Movie {
     @Id
-    private ObjectId objectId;
+    private ObjectId _id;
     private String imdbId;
     private String title;
     private String releaseDate;
     private String trailerLink;
-    private String posterLink;
+    private String poster;
     private List<String> genres;
     private List<String> backdrops;
     @DocumentReference
--- src/main/java/com/rewindo/LeafyAllies/Review.java
+++ src/main/java/com/rewindo/LeafyAllies/Review.java
@@ -7,12 +7,12 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collation = "reviews")
+@Document(collection = "reviews")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Review {
     @Id
     private ObjectId objectId;
-    private String body:
+    private String body;
 }
