15a4b8c409d88f6ce2352ad4c7d0d99188eb2c95
Amirhossein Andouhgin <amirhossein.andoohgin@gmail.com>
2023-10-22 18:31:41 +0330

    single movie controller

--- src/main/java/com/rewindo/LeafyAllies/MovieService.java
+++ src/main/java/com/rewindo/LeafyAllies/MovieService.java
@@ -1,15 +1,22 @@
 package com.rewindo.LeafyAllies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
+
     public List<Movie> allMovies(){
         return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie(ObjectId _id){
+        return movieRepository.findById(_id);
     }
 }
